d4c2eb004c91d60d851b2011695b29ac033896b1
HappyRobotProject <24eaq@notsharingmy.info>
2017-07-25 20:22:53 -0400

    changed Twython method

--- python/post_daily_trend.py
+++ python/post_daily_trend.py
@@ -13,8 +13,13 @@ consec = authKeys.iloc[0,1]
 acctok = authKeys.iloc[0,2]
 accsec = authKeys.iloc[0,3]
 
+trends = pd.read_csv("todays-trend.csv", header=0)
+topTrend = trends.iloc[0,0]
+
 twitter = tw.Twython(conkey,consec,acctok,accsec)
 daily_trend = "/Users/tim/data/images/daily-trends.png"
 print("Sending Photo")
 f = open(daily_trend, 'rb')
-twitter.update_status_with_media(status = "Enjoy this #DailyTwitterTrend infographic", media = f)
+#twitter.update_status_with_media(status = "Today's #DailyTwitterTrend is ", media = f)photo = open('/path/to/file/image.jpg', 'rb')
+response = twitter.upload_media(media=f)
+twitter.update_status(status="Today's #DailyTwitterTrend is " + topTrend + "!  Enjoy the infographic.", media_ids=[response['media_id']])
