b678f6c72b4a728c113da496149be17169def19b
HappyRobotProject <24eaq@notsharingmy.info>
2017-08-23 04:17:40 -0400

    added the plist for scheduling the task

--- macos/load-daemons.sh
+++ macos/load-daemons.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 #
 #Setup LaunchDaemons
-#sudo cp com.happyrobot.qod.plist /Library/LaunchDaemons/com.happyrobot.qod.plist
-#sudo chown root /Library/LaunchDaemons/com.happyrobot.qod.plist
-#sudo chgrp wheel /Library/LaunchDaemons/com.happyrobot.qod.plist
-#sudo launchctl load /Library/LaunchDaemons/com.happyrobot.qod.plist
-#sudo launchctl start /Library/LaunchDaemons/com.happyrobot.qod.plist
+#sudo cp com.happyrobot.quarterly.plist /Library/LaunchDaemons/com.happyrobot.quarterly.plist
+#sudo chown root /Library/LaunchDaemons/com.happyrobot.quarterly.plist
+#sudo chgrp wheel /Library/LaunchDaemons/com.happyrobot.quarterly.plist
+#sudo launchctl load /Library/LaunchDaemons/com.happyrobot.quarterly.plist
+#sudo launchctl start /Library/LaunchDaemons/com.happyrobot.quarterly.plist
